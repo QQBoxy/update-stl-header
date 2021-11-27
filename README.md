0ec92abf81768db212351984f9283a70c7810c93
QQBoxy <gs1458@gmail.com>
2021-11-27 16:18:32 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# update-stl-header
+# Update binary stl header
+
+> Update the binary STL header to `QQBoxy`
---
+++ update-stl-header.js
@@ -0,0 +1,48 @@
+
+/**************************************************
+ File: update-stl-header.js
+ Name: Update stl header
+ Explain: Update binary STL header
+ node .\update-stl-header.js -i input.stl -o output.stl
+****************************************By QQBoxy*/
+const fs = require('fs');
+const path = require('path');
+const { program } = require('commander');
+
+// Entry point
+(async () => {
+    try {
+        program
+            .version('0.0.1')
+            .option('-i, --input <string>', 'Input STL file')
+            .option('-o, --output <string>', 'Output STL file');
+        program.parse(process.argv);
+        const options = program.opts();
+        // Input file
+        const inputFile = path.resolve(__dirname, options.input);
+        // Output file
+        const outputFile = path.resolve(__dirname, options.output);
+        // File exist
+        const isExist = fs.existsSync(inputFile);
+        if (!isExist) {
+            throw "File not exist!";
+        }
+        // Read Buffer
+        const arrayBuffer = fs.readFileSync(inputFile).buffer;
+        // Read DataView
+        let view = new DataView(arrayBuffer);
+        // Edit Data
+        view.setUint32(0, 1866617169, true);
+        view.setUint32(4, 31096, true);
+        for (let i = 8; i < 80; i += 4) {
+            view.setUint32(i, 0, true);
+        }
+        // Create Buffer
+        const stlBuf = Buffer.from(arrayBuffer);
+        // Write Buffer
+        fs.writeFileSync(outputFile, stlBuf);
+    } catch (error) {
+        console.log(error);
+        console.log("Error. (Ctrl+C Exit)");
+    }
+})();
